5ee8c0f1476aa9edefe1eda0bc4a9a8d8e1ce20a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-23 09:31:52 +0100

    ModifiedM lib/mandelbrot.dart

--- lib/mandelbrot.dart
+++ lib/mandelbrot.dart
@@ -1,22 +1,31 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_shaders/flutter_shaders.dart';
+
+late final FragmentShader _shader;
 
 class MandelBrotSet extends StatelessWidget {
   const MandelBrotSet({super.key});
 
   @override
-  Widget build(BuildContext context) => CustomPaint(painter: MandelBrotPainter());
+  Widget build(BuildContext context) => ShaderBuilder(
+        builder: (context) {
+          return CustomPaint(painter: MandelBrotPainter());
+        },
+        assetKey: '',
+      );
 }
 
 class MandelBrotPainter extends CustomPainter {
-  late final FragmentShader _shader;
-
   MandelBrotPainter();
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawRect(rect, paint)
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.width, size.height),
+      Paint()..shader = _shader,
+    );
   }
 
   @override
